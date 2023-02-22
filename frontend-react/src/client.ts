770a80d6d17d5d01f5a0136d5bbb120a18912466
Rafael <rafael_guilherme@hotmai.com>
2023-02-22 08:14:17 -0300

    security changes

--- frontend-react/src/client.ts
+++ frontend-react/src/client.ts
@@ -1,14 +1,17 @@
-import { createClient } from "@sanity/client";
+//import * as dotenv from 'dotenv'
+//dotenv.config({ encoding: 'latin1' }) 
+
+import { createClient } from "@sanity/client"
 import createImageUrlBuilder from "@sanity/image-url"
 
-export const config = {
-    projectId: 'qigewvwc', //process.env.REACT_APP_SANITY_PROJECT_ID || "",
+export const client = {
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID || "",
     dataset: 'production',
     apiVersion: '2023-02-01',
     useCdn: true,
-    token: 'sksgWKgCoQ3uqS5fSWtqwp2YzpArcc0Z1tnDNRy3ghDsWSBYQlrUEq1HGgedHExJMBAxKHdkuKcg0ixYV23nAP63jUZJWOkkEmZ2DUvuwABhBu8698QgGxA8Hlbou5QI2W9Ll4RKaWyb2YuAuOZMx7xDoyIuzbr3xZBM0bbmpQmlBkrTEvHH'//process.env.REACT_APP_SANITY_TOKEN,
+    token: import.meta.env.VITE_SANITY_TOKEN,
 }
 
-export const sanityClient = createClient(config);
+export const sanityClient = createClient(client);
 
-export const urlFor = (source: any) => createImageUrlBuilder(config).image(source);
+export const urlFor = (source: any) => createImageUrlBuilder(client).image(source);
